ae0fedf43499f06102def4f0583979184a68b08c
Jacob <jtljac2@gmail.com>
2020-02-11 11:26:26 +0000

    Added Travis, added gradle wrapper

--- src/main/java/com/demmodders/factions/delayedevents/PowerIncrease.java
+++ src/main/java/com/demmodders/factions/delayedevents/PowerIncrease.java
@@ -3,7 +3,16 @@ package com.demmodders.factions.delayedevents;
 import com.demmodders.datmoddingapi.delayedexecution.delayedevents.BaseDelayedEvent;
 
 public class PowerIncrease extends BaseDelayedEvent {
+
     public PowerIncrease(int Delay) {
         super(Delay);
+    }
+
+    @Override
+    public boolean shouldRequeue(boolean hasFinished) {
+        if (hasFinished){
+
+        }
+        return true;
     }
 }
