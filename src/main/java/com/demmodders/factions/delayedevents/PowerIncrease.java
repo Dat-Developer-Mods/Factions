acbcaa2869fd444b8f9eb3bc5f7fef44b2e66451
Jacob <jtljac2@gmail.com>
2020-06-19 23:01:25 +0100

    Patched bug that caused the server to crash when a player who isn't in a faction gains power over time

--- src/main/java/com/demmodders/factions/delayedevents/PowerIncrease.java
+++ src/main/java/com/demmodders/factions/delayedevents/PowerIncrease.java
@@ -1,7 +1,6 @@
 package com.demmodders.factions.delayedevents;
 
 import com.demmodders.datmoddingapi.delayedexecution.delayedevents.BaseDelayedEvent;
-import com.demmodders.factions.faction.Faction;
 import com.demmodders.factions.faction.FactionManager;
 import com.demmodders.factions.util.FactionConfig;
 import com.demmodders.factions.util.enums.FactionRank;
@@ -10,30 +9,38 @@ import net.minecraft.entity.player.EntityPlayerMP;
 public class PowerIncrease extends BaseDelayedEvent {
     public boolean cancelled = false;
     public EntityPlayerMP player;
+    private int delay;
 
     public PowerIncrease(int Delay, EntityPlayerMP Player) {
         super(Delay);
+        delay = Delay;
         player = Player;
     }
 
     public double rankMultiplier(FactionRank rank){
-        switch (rank){
-            case GRUNT:
-                return FactionConfig.powerSubCat.powerGainGruntMultiplier;
-            case LIEUTENANT:
-                return FactionConfig.powerSubCat.powerGainLieutenantMultiplier;
-            case OFFICER:
-                return FactionConfig.powerSubCat.powerGainSergeantMultiplier;
-            case OWNER:
-                return FactionConfig.powerSubCat.powerGainOwnerMultiplier;
+        if (rank != null) {
+            switch (rank) {
+                case GRUNT:
+                    return FactionConfig.powerSubCat.powerGainGruntMultiplier;
+                case LIEUTENANT:
+                    return FactionConfig.powerSubCat.powerGainLieutenantMultiplier;
+                case OFFICER:
+                    return FactionConfig.powerSubCat.powerGainSergeantMultiplier;
+                case OWNER:
+                    return FactionConfig.powerSubCat.powerGainOwnerMultiplier;
+            }
         }
         return 1.D;
     }
 
     @Override
     public void execute() {
-        FactionManager.getInstance().getPlayer(player.getUniqueID()).addMaxPower((int) Math.ceil(FactionConfig.powerSubCat.maxPowerGainAmount * rankMultiplier(FactionManager.getInstance().getPlayer(player.getUniqueID()).factionRank)));
-        FactionManager.getInstance().getPlayer(player.getUniqueID()).addPower((int) Math.ceil(FactionConfig.powerSubCat.powerGainAmount * rankMultiplier(FactionManager.getInstance().getPlayer(player.getUniqueID()).factionRank)));
+        // Only execute when a player is in a faction
+        if (!FactionManager.getInstance().getPlayersFactionID(player.getUniqueID()).equals(FactionManager.WILDID)) {
+            FactionManager.getInstance().getPlayer(player.getUniqueID()).addMaxPower((int) Math.ceil(FactionConfig.powerSubCat.maxPowerGainAmount * rankMultiplier(FactionManager.getInstance().getPlayer(player.getUniqueID()).factionRank)));
+            FactionManager.getInstance().getPlayer(player.getUniqueID()).addPower((int) Math.ceil(FactionConfig.powerSubCat.powerGainAmount * rankMultiplier(FactionManager.getInstance().getPlayer(player.getUniqueID()).factionRank)));
+        }
+        exeTime = System.currentTimeMillis() + delay * 1000;
     }
 
     @Override
