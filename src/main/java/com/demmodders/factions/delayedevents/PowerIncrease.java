943dcfe7965573f8987010374d3fa306cc67dac5
Jacob <jtljac2@gmail.com>
2020-11-23 20:06:54 +0000

    Added message for when a player's power increases

--- src/main/java/com/demmodders/factions/delayedevents/PowerIncrease.java
+++ src/main/java/com/demmodders/factions/delayedevents/PowerIncrease.java
@@ -1,10 +1,13 @@
 package com.demmodders.factions.delayedevents;
 
 import com.demmodders.datmoddingapi.delayedexecution.delayedevents.BaseDelayedEvent;
+import com.demmodders.datmoddingapi.util.DemConstants;
 import com.demmodders.factions.faction.FactionManager;
+import com.demmodders.factions.faction.Player;
 import com.demmodders.factions.util.FactionConfig;
 import com.demmodders.factions.util.enums.FactionRank;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.text.TextComponentString;
 
 public class PowerIncrease extends BaseDelayedEvent {
     public boolean cancelled = false;
@@ -37,8 +40,10 @@ public class PowerIncrease extends BaseDelayedEvent {
     public void execute() {
         // Only execute when a player is in a faction
         if (!FactionManager.getInstance().getPlayersFactionID(player.getUniqueID()).equals(FactionManager.WILDID)) {
-            FactionManager.getInstance().getPlayer(player.getUniqueID()).addMaxPower((int) Math.ceil(FactionConfig.powerSubCat.maxPowerGainAmount * rankMultiplier(FactionManager.getInstance().getPlayer(player.getUniqueID()).factionRank)));
-            FactionManager.getInstance().getPlayer(player.getUniqueID()).addPower((int) Math.ceil(FactionConfig.powerSubCat.powerGainAmount * rankMultiplier(FactionManager.getInstance().getPlayer(player.getUniqueID()).factionRank)));
+            Player fPlayer = FactionManager.getInstance().getPlayer(this.player.getUniqueID());
+            fPlayer.addMaxPower((int) Math.ceil(FactionConfig.powerSubCat.maxPowerGainAmount * rankMultiplier(fPlayer.factionRank)));
+            fPlayer.addPower((int) Math.ceil(FactionConfig.powerSubCat.powerGainAmount * rankMultiplier(fPlayer.factionRank)));
+            this.player.sendMessage(new TextComponentString(DemConstants.TextColour.INFO + "You just gained power! Your power is now " + fPlayer.power.power + "/" + fPlayer.power.maxPower));
         }
         exeTime = System.currentTimeMillis() + delay * 1000;
     }
